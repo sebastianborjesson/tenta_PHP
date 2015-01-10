a399cb2faff6e461002c60a7eac6d7a8d4d8980f
Sebastian Börjesson <sebastian.borjesson@live.se>
2015-01-10 14:28:16 +0100

    försöker bygga challenge-generator

--- get_challenges.php
+++ get_challenges.php
@@ -10,3 +10,33 @@ $ds = new DBObjectSaver(array(
   "prefix" => "wu14oop2"
 ));
 
+$last_challenge_index = isset($_REQUEST["lastChallenge"]) ? $_REQUEST["lastChallenge"] : false;
+
+
+
+/**
+ * Pick a random challenge
+ *
+ */
+
+//if $last_challenge_index is not false (STRICT) user 
+//asked to change to another challenge
+if ($last_challenge_index !== false) {
+  //pick a random challenge index that's not the same as the last one
+  $random_challenge_index = $last_challenge_index;
+  while ($random_challenge_index == $last_challenge_index) {
+    $random_challenge_index = rand(0, count($ds->challenges)-1);
+  }
+} else {
+  //just pick any random challenge index
+  $random_challenge_index = rand(0, count($ds->challenges)-1);
+}
+
+//remove old challenge
+unset($ds->current_challenge);
+
+//add the new one
+$ds->current_challenge[] = $ds->challenges[$random_challenge_index];
+
+//and echo it out
+echo(json_encode($ds->current_challenge[0]));
--- js/main.js
+++ js/main.js
@@ -4,22 +4,22 @@ $(function() {
 		var playerName = $('.characterName').val();
 		var playerClass = ($("input[type='radio']:checked").val());
 		$.ajax({
-			url:"gameplay.php",
+			url:"start_game.php",
 			dataType:"json",
 			data: {
-			playerName: playerName,
-			playerClass: playerClass
+				playerName: playerName,
+				playerClass: playerClass
 			},
 			success: function(data) {
-				selectChallenge();
+
 				$(".characterInfo").hide();
 				for (var i = 0; i < data.length; i++) {
 					for (var j = 0; j < data[i].length; j++) {
 
 						$(".selectNewChallenge").append("<h2>Welcome " + data[i][j].name + " to the game of masters!");
 						$(".selectNewChallenge").append("<p>Pick a challenge by accepting or hit pick new challenge if you want to change</p>");
-						$(".selectNewChallenge").append("<button class='accept_challenge' type=text>Accept challenge</button>");
-						$(".selectNewChallenge").append("<button class='pick_new_challenge' type=text>Pick new challenge");
+						$(".selectNewChallenge").append("<button class='accept_challenge'>Accept challenge</button>");
+						$(".selectNewChallenge").append("<button class='pick_new_challenge'>Pick new challenge");
 						
 						console.log("YAY! stored in database", data);
 					}
@@ -31,9 +31,23 @@ $(function() {
 		});
 	});
 
-	function selectChallenge() {
-		
-	}
+	$(".pick_new_challenge").on("click", function(){
+
+
+		$.ajax({
+			url: "get_challenge.php",
+			dataType: "json",
+			data: {
+				lastChallenge: 1
+			},
+			success: function(data) {
+				console.log("You picked a random challenge", data);
+			},
+			error: function(data) {
+				console.log("It didnt work!", data.responseText);
+			}
+		});
+	});
 
 	$(".reset").click(function(){
 		$.ajax({
